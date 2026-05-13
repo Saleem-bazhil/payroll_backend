47e9d820a453f793f1172bbc8d734a060a1e4064
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-13 14:17:05 +0530

    implement geofenced attendance check-in and check-out based on employee work location coordinates upgrades 13.5.26v

--- employees/serializer.py
+++ employees/serializer.py
@@ -8,6 +8,12 @@ class EmployeeSerializer(serializers.ModelSerializer):
     def validate_phone(self, value):
         return value or None
 
+    def validate_work_lat(self, value):
+        return value if value else None
+
+    def validate_work_lon(self, value):
+        return value if value else None
+
     class Meta:
         model = Employee
         fields = '__all__'
